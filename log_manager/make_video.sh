5319887f5aa149312ab2723458efbf90b06beba5
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2021-01-11 16:55:19 +0900

    tiny fix

--- log_manager/make_video.sh
+++ log_manager/make_video.sh
@@ -12,9 +12,9 @@ ${image} rosrun trifinger_object_tracking tricamera_log_converter.py ${logdir}/c
 ${image} rosrun trifinger_object_tracking tricamera_log_converter.py ${logdir}/camera_data.dat ${logdir}/video180.avi -c camera180
 ${image} rosrun trifinger_object_tracking tricamera_log_converter.py ${logdir}/camera_data.dat ${logdir}/video300.avi -c camera300
 
-/home/cbschaff/usr/bin/ffmpeg -i ${logdir}/video60.avi -i ${logdir}/video180.avi -filter_complex hstack -q:v 1 ${logdir}/video_temp.avi
-/home/cbschaff/usr/bin/ffmpeg -i ${logdir}/video_temp.avi -i ${logdir}/video300.avi -filter_complex hstack -q:v 1 ${logdir}/video.avi
-/home/cbschaff/usr/bin/ffmpeg -i ${logdir}/video.avi ${logdir}/video.webm
+ffmpeg -i ${logdir}/video60.avi -i ${logdir}/video180.avi -filter_complex hstack -q:v 1 ${logdir}/video_temp.avi
+ffmpeg -i ${logdir}/video_temp.avi -i ${logdir}/video300.avi -filter_complex hstack -q:v 1 ${logdir}/video.avi
+ffmpeg -i ${logdir}/video.avi ${logdir}/video.webm
 
 rm ${logdir}/video60.avi
 rm ${logdir}/video180.avi
